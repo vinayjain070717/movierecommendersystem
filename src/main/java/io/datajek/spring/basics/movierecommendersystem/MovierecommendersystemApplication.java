d19ad5aaa2fa44154269f46a2e6880958c8f47e2
Vinay Jain <vinayjain703@gmail.com>
2025-03-24 23:09:05 +0530

    inital commit

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/MovierecommendersystemApplication.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommendersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommendersystemApplication.class, args);
+	}
+
+}
